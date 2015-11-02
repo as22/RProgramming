c0d08465af4e17dea072cb1e1a1b36e8651a34e4
as22 <singhal.abhi@gmail.com>
2015-11-01 17:51:01 -0800

    Added more code

--- zillow.py
+++ zillow.py
@@ -13,101 +13,120 @@ for code in all_codes:
 	print url
 	page_html = urllib.urlopen(url).read()
 	soup = BeautifulSoup(page_html,"html.parser")
-	print (soup.prettify())
-	property_listings = soup.findAll("div",{"class":"property-listing-data"})
+	total_pages = soup.findAll("ol",{"class":"zsg-pagination"})
 
-	if(len(property_listings)>0):
-
-		data_file_name = str(code)+".txt"
-		data_file = open(data_file_name,'w')
-		data_file.write("address,bed,bath,sq_ft,built,lot\n")
+	if total_pages:
+		for links in total_pages:
+			total_pages = len(links.findAll('a'))
 	else:
-		continue
+		total_pages = 1
 
-	for listing in property_listings:
-		sq_ft      = "NA,"
-		locality   = "NA,"
-		addr       = "NA,"
-		zip	       = "NA,"
-		bed_bath   = "NA,"
-		built_year = "NA,"
-		lot_size   = "NA,"
-		price	   = "NA,"
-		data       = "NA,"
-		year 	   = "NA,"
-		lot  	   = "NA,"
-		bed 	   = "NA,"
-		baths      = "NA,"
-		lot        = "NA,"
-		price      = "NA,"
-		year       = "NA,"
-		beds	   = "NA,"
+	page_number = 1
 
-		locality   = listing.find("span",{"itemprop":"addressLocality"})
-		addr       = listing.find("span",{"itemprop":"streetAddress"})
-		zip        = listing.find("span",{"itemprop":"postalCode"})
-		bed_bath   = listing.find("span",{"class":"beds-baths-sqft"})
-		built_year = listing.find("span",{"class":"built-year"})
-		lot_size   = listing.find("span",{"class":"lot-size"})
-		price      = listing.find("dt",{"class":"price-large zsg-h2 zsg-content_collapsed"})
+	while page_number < total_pages:
 
-		if addr:
-			addr =  addr.text + " "
-		else:
-			addr = "NA,"
-		if locality:
-			locality =  locality.text + " "
-		else:
-			locality = "NA,"
-		if zip:
-			zip = zip.text + ","
-		else:
-			zip = "NA,"
-		if bed_bath:
-			beds   = re.search(r'(\d+\.*\d*)\s+bds',bed_bath.text)
-			baths  = re.search(r'(\d+\.*\d*)\s+ba', bed_bath.text)
-			sq_ft  = re.search(r'([\d|\,]+)\s+sqft',bed_bath.text)
+		url = "http://www.zillow.com/homes/for_rent/"+str(code)+"_rb/"+str(page_number)+"_p/"
+		page_html = urllib.urlopen(url).read()
+		soup = BeautifulSoup(page_html,"html.parser")
+		property_listings = soup.findAll("div",{"class":"property-listing-data"})
 
-			if beds:
-				beds =  beds.group(1) + ","
+		if(len(property_listings)>0):
+			data_file_name = str(code)+".txt"
+			data_file = open(data_file_name,'a')
+
+			if(page_number ==1):
+				data_file.write("address,zip,bed,bath,sq_ft,year,lot,price,type\n")
+		elif (len(property_listings) > 0):
+			page_number = page_number + 1
+			continue
+
+		page_number = page_number + 1
+
+
+		for listing in property_listings:
+			sq_ft      = "NA"
+			locality   = "NA"
+			addr       = "NA"
+			zip	       = "NA"
+			bed_bath   = "NA"
+			built_year = "NA"
+			lot_size   = "NA"
+			price	   = "NA"
+			data       = "NA"
+			year 	   = "NA"
+			lot  	   = "NA"
+			bed 	   = "NA"
+			baths      = "NA"
+			lot        = "NA"
+			price      = "NA"
+			year       = "NA"
+			beds	   = "NA"
+			type	   = "NA"
+
+			locality   = listing.find("span",{"itemprop":"addressLocality"})
+			addr       = listing.find("span",{"itemprop":"streetAddress"})
+			zip        = listing.find("span",{"itemprop":"postalCode"})
+			bed_bath   = listing.find("span",{"class":"beds-baths-sqft"})
+			built_year = listing.find("span",{"class":"built-year"})
+			lot_size   = listing.find("span",{"class":"lot-size"})
+			price      = listing.find("dt",{"class":"price-large zsg-h2 zsg-content_collapsed"})
+			type	   = listing.find("dt",{"class":"listing-type zsg-content_collapsed"})
+
+			if addr:
+				addr =  addr.text
 			else:
-				beds =  "NA,"
-			if baths:
-				baths = baths.group(1) + ","
+				addr = "NA"
+			if locality:
+				locality =  locality.text
 			else:
-				baths =  "NA,"
-			if sq_ft:
-				sq_ft = sq_ft.group(1)
-				sq_ft = sq_ft.replace(',','')
-				sq_ft = sq_ft + ","
-			else: sq_ft =  "NA,"
-	
-		if built_year:
-			year = re.search(r'Built\s+(\d+)',built_year.text)
-			if year:
-				year = year.group(1) + ","
+				locality = "NA"
+			if zip:
+				zip = zip.text
 			else:
-				year= "NA,"
-		if lot_size:
-			lot  = re.search(r'([\d|\,]+)\s+sqft\s+lot',lot_size.text)
-			if lot:
-				lot = lot.group(1)
-				lot = lot.replace(',','')
-				lot = lot + ","
-			else:
-				lot = "NA,"
+				zip = "NA"
+			if bed_bath:
+				beds   = re.search(r'(\d+\.*\d*)\s+bds',bed_bath.text)
+				baths  = re.search(r'(\d+\.*\d*)\s+ba', bed_bath.text)
+				sq_ft  = re.search(r'([\d|\,]+)\s+sqft',bed_bath.text)
 
-		if price:
-			price = re.search(r'\$([\d|\,]+)\/mo',price.text)
+				if beds:
+					beds =  beds.group(1)
+				else:
+					beds =  "NA"
+				if baths:
+					baths = baths.group(1)
+				else:
+					baths =  "NA"
+				if sq_ft:
+					sq_ft = sq_ft.group(1)
+					sq_ft = sq_ft.replace(',','')
+				else: sq_ft =  "NA"
+
+			if built_year:
+				year = re.search(r'Built\s+(\d+)',built_year.text)
+				if year:
+					year = year.group(1)
+				else:
+					year= "NA"
+			if lot_size:
+				lot  = re.search(r'([\d|\,]+)\s+sqft\s+lot',lot_size.text)
+				if lot:
+					lot = lot.group(1)
+					lot = lot.replace(',','')
+				else:
+					lot = "NA"
+
 			if price:
-				price = price.group(1)
-				price = price.replace(',','')
-				price = price + ","
-			else: price = "NA,"
 
-		if listing:
-			#data = addr + locality + zip + beds + baths + year +  sq_ft + lot + price
-			print addr, locality, zip, beds,baths,year,sq_ft,lot,price
-			#data_file.write(data.encode('utf-8'))
-			#data_file.write(string)
-			#data_file.write("\n")
+				price = re.search(r'(\$[\d|\,]+)\/mo',price.text)
+
+				if price:
+					price = price.group(1)
+					price = price.replace(',','')
+				else: price = "NA"
+
+			if type:
+					type = type.contents[1]
+
+			if listing:
+				data_file.write("%s %s,%s,%s,%s,%s,%s,%s,%s,%s\n" %(addr,locality,zip,beds,baths,year,sq_ft,lot,price,type))
